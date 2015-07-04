3d18002d9b76b89c488324e776e36f9f2814c8ec
Steven Githens <swgithen@mtu.edu>
2015-07-04 12:24:37 +0200

    Fixed up loading argument sources.

--- lgs-station/lgs_driver.js
+++ lgs-station/lgs_driver.js
@@ -1,19 +1,14 @@
 "use strict";
 
-// var fluid = require("infusion"),
-//      kettle = fluid.registerNamespace("kettle");
-//
-// require("kettle");
-// require("./index.js");
-
 var fluid = require("universal"),
-    gpii = fluid.registerNamespace("gpii");
+    gpii = fluid.registerNamespace("gpii"),
+    kettle = fluid.registerNamespace("kettle");
 
 require("./index.js");
 
-gpii.start();
+//gpii.start();
 
-// kettle.config.makeConfigLoader({
-//     configName: kettle.config.getNodeEnv("lgsConfig"),
-//     configPath: kettle.config.getConfigPath() || __dirname
-// });
+kettle.config.makeConfigLoader({
+    configName: kettle.config.getNodeEnv("lgsConfig"),
+    configPath: kettle.config.getConfigPath() || __dirname
+});
